b4602973261e1b541f8c2f2f0d3fb98f52ea8381
Vishwa Gaurav <vishwagauravin@gmail.com>
2022-05-15 01:22:42 +0530

    added title bar for center taskbar items

--- components/taskbar/taskbar-components/center/Icon.js
+++ components/taskbar/taskbar-components/center/Icon.js
@@ -1,13 +1,16 @@
 import React from "react";
 
-export default function Icon({ name }) {
+export default function Icon({ name, title }) {
   return (
     <>
-      <div className="w-10 aspect-square rounded hover:bg-white/10 select-none hover:ring-1 ring-white/5 p-2">
+      <div className="w-10 group aspect-square rounded hover:bg-white/10 select-none hover:ring-1 ring-white/5 p-2">
         <img src={`/icons/${name}.png`} alt="" />
-      </div>
       {/* Overlay */}
-      
+      <div className="hidden absolute group-hover:flex -mt-[4.6rem] bg-[#050E74] px-3 p-1.5 select-none hover:ring-1 ring-white/5 rounded text-sm fadein">
+          <p className="w-max">{title}</p>
+      </div>
+      </div>
+
     </>
   );
 }
--- components/taskbar/taskbar-components/center/Icons.js
+++ components/taskbar/taskbar-components/center/Icons.js
@@ -4,10 +4,10 @@ import Icon from './Icon'
 export default function Icons() {
   return (
     <div className='flex absolute left-1/2 -translate-x-1/2 space-x-1'>
-        <Icon name="start"/>
-        <Icon name="edge"/>
-        <Icon name="vsc"/>
-        <Icon name="fe"/>
+        <Icon name="start" title="Start"/>
+        <Icon name="edge" title="Microsoft Edge"/>
+        <Icon name="vsc" title="Visual Studio Code"/>
+        <Icon name="fe" title="File Explorer"/>
     </div>
   )
 }
