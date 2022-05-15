b9142787799babcfa384cd9e40a8a8f91b519001
Vishwa Gaurav <vishwagauravin@gmail.com>
2022-05-16 01:16:18 +0530

    added searchbar to widgets

--- components/taskbar/taskbar-components/widget/Widgets.js
+++ components/taskbar/taskbar-components/widget/Widgets.js
@@ -1,3 +1,4 @@
+import { SearchIcon } from "@heroicons/react/outline";
 import { PlusIcon, UserIcon } from "@heroicons/react/solid";
 import React from "react";
 
@@ -6,15 +7,20 @@ export default function Widgets() {
     <div className="absolute z-20 h-[835px] w-[750px] bg-gray-700/90 top-0 left-4 -mt-[800px] rounded-lg text-gray-200">
       {/* Header */}
       <div className="relative">
-        <p className="absolute left-1/2 -translate-x-1/2 text-3xl mt-6">
+        <p className="absolute left-1/2 -translate-x-1/2 text-3xl mt-5">
           15:44
         </p>
-        <div className="w-[88px] h-12 bg-gray-600/90 absolute right-4 top-4 rounded-md flex justify-center items-center">
-          <PlusIcon className="w-7" />
-          <div className="rounded-full w-8 aspect-square bg-zinc-200 ml-1 flex justify-center items-center">
-            <UserIcon className="w-6 fill-zinc-700" />
+        <div className="w-[88px] h-11 bg-gray-600/90 absolute right-4 top-4 rounded-md flex justify-center items-center">
+          <PlusIcon className="w-6" />
+          <div className="rounded-full w-7 aspect-square bg-zinc-200 ml-1 flex justify-center items-center">
+            <UserIcon className="w-5 fill-zinc-700" />
           </div>
         </div>
+      </div>
+      {/* Search Bar */}
+      <div className="w-[615px] h-9 bg-zinc-800/90 mt-20 relative left-1/2 -translate-x-1/2 rounded flex items-center">
+        <SearchIcon className="w-5 ml-2"/>
+        <input type="text" name="" id="" className="h-7 bg-transparent ml-1 w-11/12 outline-none" placeholder="Search the web" autoFocus={true}/>
       </div>
     </div>
   );
