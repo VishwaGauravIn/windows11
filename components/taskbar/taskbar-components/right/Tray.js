c5e9a89fe93d1a4c051efa267b280a28b4c66af3
Vishwa Gaurav <vishwagauravin@gmail.com>
2022-05-15 14:21:18 +0530

    minor impv

--- components/taskbar/taskbar-components/right/Tray.js
+++ components/taskbar/taskbar-components/right/Tray.js
@@ -5,13 +5,14 @@ export default function Tray() {
   return (
     <div className="absolute right-0 mr-5 opacity-95 flex flex-wrap">
       {/* Date-Time and DND */}
-      <div className="">
+      <div className="flex">
         <div className="text-xs mr-2">
           <p className="text-right">14:16</p>
           <p className="">15-05-2022</p>
         </div>
         <MoonIcon className="w-5 stroke-[1.5] -rotate-90" />
       </div>
+      
     </div>
   );
 }
