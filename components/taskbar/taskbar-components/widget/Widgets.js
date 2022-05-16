c15c6e820d1aed6180c3d7ef11e79909bf1399e9
Vishwa Gaurav <vishwagauravin@gmail.com>
2022-05-16 12:00:46 +0530

    added stock widget

--- components/taskbar/taskbar-components/widget/Widgets.js
+++ components/taskbar/taskbar-components/widget/Widgets.js
@@ -67,7 +67,38 @@ export default function Widgets() {
               See more about IPL
             </p>
           </div>
-          
+          {/* Stock Card */}
+          <div className="w-[301px] bg-zinc-800/80 h-[145px] rounded p-4">
+            <div className="flex relative">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="w-5 mr-2 stroke-green-400"
+                viewBox="0 0 24 24"
+                strokeWidth={2}
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M13 7h8m0 0v8m0-8l-8 8-4-4-6 6"
+                />
+              </svg>
+              <p className="opacity-90">Stock Market</p>
+              <DotsHorizontalIcon className="w-5 mt-0.5 absolute right-0" />
+            </div>
+            <div className="px-4 pt-4 opacity-90">
+              <span className="flex justify-between">
+                <p className="">Apple Inc.</p>
+                <p className="">+1.35%</p>
+              </span>
+              <span className="flex justify-between">
+                <p className="">Tata Motors</p>
+                <p className="">+0.35%</p>
+              </span>
+            </div>
+            <p className="text-sm mt-2 text-center text-cyan-500 opacity-80">
+              Check stock prices
+            </p>
+          </div>
         </div>
       </div>
     </div>
