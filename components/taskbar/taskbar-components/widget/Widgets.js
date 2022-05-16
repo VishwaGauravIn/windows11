85bd4584e25ff3a2b78cf9f228ae9e0041183617
Vishwa Gaurav <vishwagauravin@gmail.com>
2022-05-16 12:00:29 +0530

    added sports widget

--- components/taskbar/taskbar-components/widget/Widgets.js
+++ components/taskbar/taskbar-components/widget/Widgets.js
@@ -1,4 +1,4 @@
-import { SearchIcon } from "@heroicons/react/outline";
+import { DotsHorizontalIcon, SearchIcon } from "@heroicons/react/outline";
 import { PlusIcon, UserIcon } from "@heroicons/react/solid";
 import React from "react";
 
@@ -19,8 +19,56 @@ export default function Widgets() {
       </div>
       {/* Search Bar */}
       <div className="w-[615px] h-9 bg-zinc-800/90 mt-20 relative left-1/2 -translate-x-1/2 rounded flex items-center">
-        <SearchIcon className="w-5 ml-2"/>
-        <input type="text" name="" id="" className="h-7 bg-transparent ml-1 w-11/12 outline-none" placeholder="Search the web" autoFocus={true}/>
+        <SearchIcon className="w-5 ml-2" />
+        <input
+          type="text"
+          name=""
+          id=""
+          className="h-7 bg-transparent ml-1 w-11/12 outline-none"
+          placeholder="Search the web"
+          autoFocus={true}
+        />
+      </div>
+      {/* Widgets */}
+      <div className="w-[615px] relative left-1/2 -translate-x-1/2 mt-8">
+        {/* Stack 1 */}
+        <div className="flex justify-between">
+          {/* Sports Card */}
+          <div className="w-[301px] bg-zinc-800/80 h-[145px] rounded p-4">
+            <div className="flex relative">
+              <img src="/icons/ipl.png" alt="" className="w-4 mr-3" />
+              <p className="opacity-90">Indian Premier League</p>
+              <DotsHorizontalIcon className="w-5 mt-0.5 absolute right-0" />
+            </div>
+            {/* Scores */}
+            <div className="flex mt-5 relative">
+              <img
+                src="/icons/csk.png"
+                alt=""
+                className="w-[52px] opacity-80"
+              />
+              <div className="text-sm ml-1 mt-2 opacity-80">
+                <p className="">200/7</p>
+                <p className="">20.0 overs</p>
+              </div>
+
+              <img
+                src="/icons/mi.png"
+                alt=""
+                className="w-[52px] opacity-80 h-10 absolute right-0 mt-1"
+              />
+              <div className="text-sm ml-6 mt-2 opacity-80">
+                <p className="text-right">138/7</p>
+                <p className="">16.8 overs</p>
+              </div>
+            </div>
+            {/* For Extras */}
+            <p className="text-sm mt-2 text-center text-cyan-500 opacity-80">
+              See more about IPL
+            </p>
+          </div>
+          
+        </div>
       </div>
     </div>
   );
