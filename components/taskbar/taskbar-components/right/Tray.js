9d7a2b4fe85ae6f05c264d834cc9fa730695a9a3
Vishwa Gaurav <vishwagauravin@gmail.com>
2022-05-19 10:15:07 +0530

    made tray functional

--- components/taskbar/taskbar-components/right/Tray.js
+++ components/taskbar/taskbar-components/right/Tray.js
@@ -16,7 +16,7 @@ export default function Tray() {
       <ChevronUpIcon className="w-[24px] stroke-[1] hover:bg-white/10 h-12 px-0.5" />
       <RefreshIcon className="w-[24px] stroke-[1] hover:bg-white/10 h-12 -scale-x-100 px-0.5" />
       {/* Tray Icons (post) */}
-      <div className="flex flex-wrap space-x-2 px-2 ml-1 rounded hover:bg-white/10 select-none hover:ring-1 ring-white/5 py-2.5">
+      <div className="flex flex-wrap space-x-2 px-2 ml-1 rounded hover:bg-white/10 select-none hover:ring-1 ring-white/5 py-2.5 group">
         <WifiIcon className="w-[18px] stroke-[2]" />
         <VolumeOffIcon className="w-[18px] stroke-[2]" />
         <svg
@@ -27,6 +27,7 @@ export default function Tray() {
           <path d="M2 6h5v4H2V6z" />
           <path d="M2 4a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2H2zm10 1a1 1 0 0 1 1 1v4a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V6a1 1 0 0 1 1-1h10zm4 3a1.5 1.5 0 0 1-1.5 1.5v-3A1.5 1.5 0 0 1 16 8z" />
         </svg>
+      <TrayItems />
       </div>
       {/* Date-Time and DND */}
       <div className="flex rounded hover:bg-white/10 select-none hover:ring-1 ring-white/5 mr-2 py-0.5 px-2">
@@ -42,7 +43,6 @@ export default function Tray() {
         </div>
         <MoonIcon className="w-5 stroke-[1.5] -rotate-90" />
       </div>
-      <TrayItems />
     </div>
   );
 }
