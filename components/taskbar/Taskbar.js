d95017e63f20f2a8cc2923caa5edbb478fc74606
zadigg <62830716+zadigg@users.noreply.github.com>
2022-06-15 21:04:30 +0300

    making the taskbar to be sticky in any resolution

--- components/taskbar/Taskbar.js
+++ components/taskbar/Taskbar.js
@@ -1,14 +1,14 @@
-import React from 'react'
-import Icons from './taskbar-components/center/Icons'
-import Tray from './taskbar-components/right/Tray'
-import Weather from './taskbar-components/widget/Weather'
+import React from "react";
+import Icons from "./taskbar-components/center/Icons";
+import Tray from "./taskbar-components/right/Tray";
+import Weather from "./taskbar-components/widget/Weather";
 
 export default function Taskbar() {
   return (
-    <div className='absolute -mt-12 h-12 w-[1536px] bg-[#030C1B] flex items-center'>
-        <Weather/>
-        <Icons/>
-        <Tray/>
+    <div className="absolute top-[100vh] -mt-12 h-12 w-[100vw] bg-[#030C1B] flex items-center">
+      <Weather />
+      <Icons />
+      <Tray />
     </div>
-  )
+  );
 }
