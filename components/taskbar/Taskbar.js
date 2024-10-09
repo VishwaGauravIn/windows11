fd8cda3a7f521ff21ea5c8f82d263daf9cd38415
Esteban Carrillo <75136217+EstebanCarrilloG@users.noreply.github.com>
2024-10-09 18:46:59 -0500

    Update Taskbar.js
    Fixing taskbar not visible

--- components/taskbar/Taskbar.js
+++ components/taskbar/Taskbar.js
@@ -5,7 +5,7 @@ import Weather from './taskbar-components/widget/Weather'
 
 export default function Taskbar() {
   return (
-    <div className='absolute -mt-12 h-12 w-[1536px] bg-[#030C1B] flex items-center'>
+    <div className='absolute -mt-12 h-12 w-[1536px] bg-[#030C1B] flex items-center bottom-0'>
         <Weather/>
         <Icons/>
         <Tray/>
